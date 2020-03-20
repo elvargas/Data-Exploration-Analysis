2bfc002368f341341fb28967d19704b3ba453933
Eric Vargas <vargas.eric7@yahoo.com>
2020-03-19 21:00:44 -0500

    Add files via upload

---
+++ week2EVargas.py
@@ -0,0 +1,38 @@
+# Assignment 2.1
+# Eric Vargas
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+# Add two numbers together
+x = 2
+y = 3
+z = x + y
+print(z)
+
+# Subtract a number from another number
+a = 10
+b = 3
+c = a - b
+print(c)
+
+# Multiply two numbers
+print(32 * 2)
+
+# Divide between two numbers
+print(63/7)
+
+# Concatenate two strings together (any words)
+word1 = "Good"
+word2 = "Bye"
+print(word1 + word2)
+
+# Create a list of 4 items (can be strings, numbers, both)
+fruit = ["apple", "pears", "oranges", "bananas"]
+# Append an item to your list (again, can be a string, number)
+fruit.append("grapes")
+print(fruit)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+cars = ("chevy", "dodge", "ford", "jeep")
+print(cars)
